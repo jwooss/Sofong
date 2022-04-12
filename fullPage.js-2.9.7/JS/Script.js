03a5df613d50b0f109836f6ec796f1516693584d
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2022-04-12 15:29:44 +0900

    [Fix] FE 오류 수정

--- fullPage.js-2.9.7/JS/Script.js
+++ fullPage.js-2.9.7/JS/Script.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         sectionsColor: ['#FFFAD0', '#FFF7AA', '#FFFAD0', '#FFF7AA', '#FFFAD0'],
         css3: true,
-        anchors: ['part1','part2','p art3','part4','part5'],
-        navigationTooltips:['1','2','3','4','5'],
-        showActiveTooltip: true,
+        anchors: ['Sec1','Sec2','Sec3','Sec4','Sec5'],
         navigation: true,
+        slidesNavigation: true
+
+        // navigationTooltips:['1','2','3','4','5'],
+        // showActiveTooltip: true,
     });
 });
 

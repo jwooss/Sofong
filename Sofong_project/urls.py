5ea793ff9a916dbdb0136e2664ad7a6115f0b962
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-04-27 13:28:14 +0900

    [Updata] : 주석

--- Sofong_project/urls.py
+++ Sofong_project/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('Sofong_apps/', include('Sofong_apps.urls')),
+    # path('Sofong_apps/', include('Sofong_apps.urls')),
     path('admin/', admin.site.urls)
 ]

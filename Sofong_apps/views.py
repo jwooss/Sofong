a7a6dd778dc8cb3ac776d4141cc4c29aa8dc7ba7
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-04-28 12:19:36 +0900

    [Updata] : FE 연결 Test

--- Sofong_apps/views.py
+++ Sofong_apps/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hi, Sofong main page!")
+    return render(request, 'FE/index.html', {})
--- Sofong_project/urls.py
+++ Sofong_project/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
-    # path('Sofong_apps/', include('Sofong_apps.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', include('Sofong_apps.urls'))
 ]
